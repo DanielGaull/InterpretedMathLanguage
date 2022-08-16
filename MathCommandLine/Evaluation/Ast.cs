471487e3d429c6d940adc6d76977c339c797a25f
DanielGaull <danieljgaull@gmail.com>
2022-08-16 16:09:09 -0400

    Added missing eval files

---
+++ MathCommandLine/Evaluation/Ast.cs
@@ -0,0 +1,47 @@
+﻿using MathCommandLine.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathCommandLine.Evaluation
+{
+    public class Ast
+    {
+        public AstTypes Type { get; private set; }
+        // Used for Literal types
+        public MValue ValueArg { get; private set; }
+        // The array of arguments. Used for Function types
+        // Because everything breaks down to functions, literals, and variables, functions are the only way a tree is generated
+        public Ast[] ArgumentArray { get; private set; }
+        // Used for both Function and Variable types
+        public string Name { get; private set; }
+
+        private Ast(AstTypes type, MValue valueArg, Ast[] argumentArray, string name)
+        {
+            Type = type;
+            ValueArg = valueArg;
+            ArgumentArray = argumentArray;
+            Name = name;
+        }
+
+        public static Ast Literal(MValue value)
+        {
+            return new Ast(AstTypes.LiteralValue, value, null, null);
+        }
+        public static Ast Function(string name, params Ast[] args)
+        {
+            return new Ast(AstTypes.Function, MValue.Empty, args, name);
+        }
+        public static Ast Variable(string name)
+        {
+            return new Ast(AstTypes.Variable, MValue.Empty, null, name);
+        }
+    }
+
+    public enum AstTypes
+    {
+        LiteralValue,
+        Function,
+        Variable,
+    }
+}
---
+++ MathCommandLine/Functions/FunctionDict.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathCommandLine.Functions
+{
+    public class FunctionDict
+    {
+        private Dictionary<string, MFunction> internalDict;
+
+        public FunctionDict(params MFunction[] functions)
+        {
+            internalDict = new Dictionary<string, MFunction>();
+            AddFunctions(functions);
+        }
+
+        public void AddFunctions(List<MFunction> functions)
+        {
+            for (int i = 0; i < functions.Count; i++)
+            {
+                internalDict.Add(functions[i].Name, functions[i]);
+            }
+        }
+        public void AddFunctions(params MFunction[] functions)
+        {
+            for (int i = 0; i < functions.Length; i++)
+            {
+                internalDict.Add(functions[i].Name, functions[i]);
+            }
+        }
+
+        public MFunction GetFunction(string name)
+        {
+            if (internalDict.ContainsKey(name))
+            {
+                return internalDict[name];
+            }
+            // TODO: Return function doesn't exist errors
+            return null;
+        }
+    }
+}
