2fe769dbd8a37fcb674b9d94c8c91914046ae176
DanielGaull <danieljgaull@gmail.com>
2022-08-17 10:45:28 -0400

    .

---
+++ MathCommandLine/Exceptions/InvalidParseException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathCommandLine.Exceptions
+{
+    public class InvalidParseException : Exception
+    {
+        public InvalidParseException(string expression)
+            : base("Cannot parse expression: \"" + expression + "\".")
+        {
+
+        }
+    }
+}
