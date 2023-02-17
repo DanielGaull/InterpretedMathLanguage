ef7496c7e3964b9b2cdcea9bc21e3f1af446bd8d
Daniel Gaull <danieljgaull@gmail.com>
2023-02-17 13:55:16 -0500

    Start of closures

---
+++ MathCommandLine/Environments/MEnvironment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathCommandLine.Environments
+{
+    public class MEnvironment
+    {
+        // TODO: Write this class
+    }
+}
