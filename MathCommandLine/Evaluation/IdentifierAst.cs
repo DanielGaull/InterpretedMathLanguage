5a614d17a145df9007ae94113f511f3fe23c256f
Daniel Gaull <danieljgaull@gmail.com>
2023-09-28 20:52:59 -0400

    Added IdentifierAst class

---
+++ MathCommandLine/Evaluation/IdentifierAst.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathCommandLine.Evaluation
+{
+    public class IdentifierAst
+    {
+        public IdentifierAstTypes Type { get; private set; }
+        // These two are only used for the DotAccess type
+        public Ast Parent { get; private set; }
+        // Name used for RawVar and Parent
+        public string Name { get; private set; }
+        // Used for Dereference types
+        public Ast Reference { get; private set; }
+
+        private IdentifierAst(IdentifierAstTypes type, Ast parent, string name, Ast reference)
+        {
+            Type = type;
+            Parent = parent;
+            Name = name;
+            Reference = reference;
+        }
+
+        public static IdentifierAst RawVariable(string name)
+        {
+            return new IdentifierAst(IdentifierAstTypes.RawVar, null, name, null);
+        }
+        public static IdentifierAst DotAccess(Ast parent, string name)
+        {
+            return new IdentifierAst(IdentifierAstTypes.DotAccess, parent, name, null);
+        }
+        public static IdentifierAst Dereference(Ast reference)
+        {
+            return new IdentifierAst(IdentifierAstTypes.Dereference, null, null, reference);
+        }
+    }
+
+    public enum IdentifierAstTypes
+    {
+        RawVar,
+        DotAccess,
+        Dereference,
+    }
+}
