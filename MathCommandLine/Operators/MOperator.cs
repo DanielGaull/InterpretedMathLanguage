4e4581d9c551c01c15fe119cdf712771d5d2fc11
Daniel Gaull <danieljgaull@gmail.com>
2023-09-30 16:52:54 -0400

    Added some operator classes

---
+++ MathCommandLine/Operators/MOperator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathCommandLine.Operators
+{
+    // Class that represents an operator
+    // This is without behavior defined; just that the operator exists
+    public class MOperator
+    {
+        // Note: for ternary, splits the string in half to get the 'before' and 'after'
+        public string CodeString { get; private set; }
+        public string Name { get; private set; }
+        // 1 = unary, 2 = binary, 3 = ternary
+        public int Tier { get; private set; }
+
+        private MOperator(string code, string name, int tier)
+        { 
+            CodeString = code;
+            Name = name;
+            Tier = tier;
+        }
+
+        public static List<MOperator> GetOperators()
+        {
+            return new List<MOperator>()
+            {
+                new MOperator("+", "add", 2),
+                new MOperator("-", "subtract", 2),
+                new MOperator("*", "multiply", 2),
+                new MOperator("/", "divide", 2),
+                new MOperator("%", "remainder", 2),
+
+                new MOperator("&", "bit_and", 2),
+                new MOperator("|", "bit_or", 2),
+                new MOperator("~", "bit_not", 1),
+
+                new MOperator("&&", "and", 2),
+                new MOperator("||", "or", 2),
+                new MOperator("!", "not", 1),
+
+                new MOperator("<<", "bitshift_left", 2),
+                new MOperator(">>", "bitshift_right", 2),
+
+                new MOperator("==", "equals", 2),
+                new MOperator("!=", "not_equals", 2),
+                new MOperator("<", "less", 2),
+                new MOperator(">", "greater", 2),
+                new MOperator("<=", "less_equal", 2),
+                new MOperator(">=", "greater_equal", 2),
+
+                new MOperator("?:", "conditional", 3),
+            };
+        }
+    }
+}
