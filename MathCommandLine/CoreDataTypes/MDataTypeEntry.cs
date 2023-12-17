7893e641b9127f9d54e471d393adb3f8c21c603c
Daniel Gaull <danieljgaull@gmail.com>
2023-12-16 22:12:07 -0500

    Restructured generic type stuff'

--- MathCommandLine/CoreDataTypes/MDataTypeEntry.cs
+++ MathCommandLine/CoreDataTypes/MDataTypeEntry.cs
@@ -4,44 +4,81 @@ using System.Text;
 
 namespace IML.CoreDataTypes
 {
-    public class MDataTypeEntry
+    public abstract class MDataTypeEntry
+    {
+        public static readonly MDataTypeEntry Any = new MConcreteDataTypeEntry(MDataType.Any);
+        public static readonly MDataTypeEntry Number = new MConcreteDataTypeEntry(MDataType.Number);
+        public static readonly MDataTypeEntry Boolean = new MConcreteDataTypeEntry(MDataType.Boolean);
+        public static readonly MDataTypeEntry String = new MConcreteDataTypeEntry(MDataType.String);
+        public static readonly MDataTypeEntry Type = new MConcreteDataTypeEntry(MDataType.Type);
+        public static readonly MDataTypeEntry Error = new MConcreteDataTypeEntry(MDataType.Error);
+        public static readonly MDataTypeEntry Void = new MConcreteDataTypeEntry(MDataType.Null);
+        public static readonly MDataTypeEntry Null = new MConcreteDataTypeEntry(MDataType.Void);
+        public static MDataTypeEntry List(MType of)
+        {
+            return new MConcreteDataTypeEntry(MDataType.List, of);
+        }
+        public static MDataTypeEntry Reference(MType of)
+        {
+            return new MConcreteDataTypeEntry(MDataType.Reference, of);
+        }
+        public static MDataTypeEntry Fuction(MType returnType, params MType[] paramTypes)
+        {
+            return new MFunctionDataTypeEntry(returnType, new List<MType>(paramTypes), new List<string>());
+        }
+        public static MDataTypeEntry Fuction(MType returnType, List<MType> paramTypes)
+        {
+            return new MFunctionDataTypeEntry(returnType, paramTypes, new List<string>());
+        }
+        public static MDataTypeEntry Fuction(MType returnType, List<MType> paramTypes, List<string> genericNames)
+        {
+            return new MFunctionDataTypeEntry(returnType, paramTypes, genericNames);
+        }
+    }
+    public class MConcreteDataTypeEntry : MDataTypeEntry
     {
         public MDataType DataType { get; private set; }
         public List<MType> Generics { get; private set; }
 
-        public MDataTypeEntry(MDataType dt)
+        public MConcreteDataTypeEntry(MDataType dt)
         {
             DataType = dt;
             Generics = new List<MType>();
         }
-        public MDataTypeEntry(MDataType dt, List<MType> generics)
+        public MConcreteDataTypeEntry(MDataType dt, List<MType> generics)
         {
             DataType = dt;
             Generics = generics;
         }
-        public MDataTypeEntry(MDataType dt, params MType[] generics)
+        public MConcreteDataTypeEntry(MDataType dt, params MType[] generics)
         {
             DataType = dt;
             Generics = new List<MType>(generics);
         }
+    }
+    public class MFunctionDataTypeEntry : MConcreteDataTypeEntry
+    {
+        public MType ReturnType { get; private set; }
+        public List<MType> ParameterTypes { get; private set; }
+        public List<string> GenericNames { get; private set; }
 
-        public static readonly MDataTypeEntry Any = new MDataTypeEntry(MDataType.Any);
-        public static readonly MDataTypeEntry Number = new MDataTypeEntry(MDataType.Number);
-        public static readonly MDataTypeEntry Boolean = new MDataTypeEntry(MDataType.Boolean);
-        public static readonly MDataTypeEntry String = new MDataTypeEntry(MDataType.String);
-        public static readonly MDataTypeEntry Type = new MDataTypeEntry(MDataType.Type);
-        public static readonly MDataTypeEntry Error = new MDataTypeEntry(MDataType.Error);
-        public static readonly MDataTypeEntry Void = new MDataTypeEntry(MDataType.Null);
-        public static readonly MDataTypeEntry Null = new MDataTypeEntry(MDataType.Void);
-        public static MDataTypeEntry List(MType of)
+        public MFunctionDataTypeEntry(MType returnType, List<MType> paramTypes, List<string> genericNames)
+            : base(MDataType.Function)
         {
-            return new MDataTypeEntry(MDataType.List, of);
+            ReturnType = returnType;
+            ParameterTypes = paramTypes;
+            GenericNames = genericNames;
         }
-        public static MDataTypeEntry Reference(MType of)
+    }
+    public class MGenericDataTypeEntry
+    {
+        // A generic type that isn't resolved yet
+        // Simply has a name
+        // This type gets resolved when used later
+        public string Name { get; set; }
+        public MGenericDataTypeEntry(string name)
         {
-            return new MDataTypeEntry(MDataType.Reference, of);
+            Name = name;
         }
-
-
     }
 }
